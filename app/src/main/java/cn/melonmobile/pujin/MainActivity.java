bbded4231717149e76c9f1d37b83339b3b34e332
yqr <138920428@qq.com>
2018-07-17 10:16:19 +0800

    原生分享换了一下包名

--- app/src/main/java/cn/melonmobile/pujin/MainActivity.java
+++ app/src/main/java/cn/melonmobile/pujin/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.hyz.sharedemo.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
