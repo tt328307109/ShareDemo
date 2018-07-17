5a8cd194cda20a625c963964e1564669a44d79c6
yqr <138920428@qq.com>
2018-07-17 16:12:52 +0800

    友盟分享说明

--- app/src/main/java/cn/melonmobile/pujin/wxapi/WXEntryActivity.java
+++ app/src/main/java/cn/melonmobile/pujin/wxapi/WXEntryActivity.java
@@ -6,8 +6,7 @@ import com.umeng.socialize.weixin.view.WXCallbackActivity;
  * Created by tanghuan on 2018/4/12.
  * Supported By 甜瓜移动.
  * Official Website: www.melonmobile.cn.
- *
- * @author tanghuan
+ * 这里必须要在wxapi这个包下面
  */
 public class WXEntryActivity extends WXCallbackActivity {
 }
